8185d137a3601012e99bde0f7cc600bf66c8239f
Simon Llewellyn <Simon.Llewellyn@live.uwe.ac.uk>
2017-11-23 14:06:55 +0000

    updated to more recurive funtionality

--- Practical 5/src/practical/pkg5/Practical5.java
+++ Practical 5/src/practical/pkg5/Practical5.java
@@ -27,122 +27,62 @@ public class Practical5 {
         int index = 0;
 
       String alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+      
+      
 //         char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
 //                            'n','o','p','q','r','s','t','u','v','w','x','y','z',
-//                            '0','1','2','3','4','5','6','7','8','9'};
+//                            '0','1','2','3','4','5','6','7','8','9'   };
        
 
 //String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
 //            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 //            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
-        //  new_string(s, original, alphabet, index);
-           String test = nextString(s, alphabet, index);
-    //    new_string(s, original, alphabet, index);
+       
+       while(!s.equals(original)) {
+           s = nextString(s, alphabet, index++);
+           System.out.println(s);
+        }
 
-    
-
+   
     }
-    /////////////////////////////////////////////
-//  public static String getnext(String w){
-//      System.out.println(w);
-//      int n;
-//      char c;
-//      n = w.length();
-//      
-//      if(n ==0) return "a";
-//      if(w.charAt(n-1) <'z'){
-//          c = (char)((int)w.charAt(n-1)+1);
-//          return w.substring(0,n-1)+String.valueOf(c);
-//      }
-//      else{
-//          
-//          return getnext("a") + "a";
-//      }
-//  }
-    
+
     
     ////////////////////////////////////////////
     public static String nextString(String s, String alphabet , int index ){
-        System.out.println(s);
+        index = index % alphabet.length(); // only ever 0-35 = to pos in alphabet
         int n  = s.length();
         String lhs = "", rhs = "";
    
+        // returns a if empty string
         if(n == 0) {
-              s += alphabet.charAt(n);         
-             return nextString( s, alphabet , ++index);
+              return  "" + alphabet.charAt(index);                  
         }
                
      
+        // if string larger than 2 split into lhs an drhs
+        // eg s = "simon", n = 5, lhs = "simo", rhs = "n"
         if(n>1){
-            lhs = s.substring(0, n-2);               
+            lhs = s.substring(0, n-1);               
         }
         rhs = s.substring(n-1);
         
         
-        
+        // if check rhs = "9" 
         if(rhs.equals(alphabet.substring(alphabet.length()-1))){            
-             rhs = "" + alphabet.charAt(0);
-             //curenrelt loops here as it will constantly send a "" as the lhs
-            return nextString(lhs,alphabet, 0) + rhs;
+             rhs = "" + alphabet.charAt(index);
+             // unable to keep track of lhs position in call
+             //            
+             return nextString(lhs, alphabet, index) + rhs;
         }
         else{
-            rhs = "" + alphabet.charAt(index);
-            return nextString(rhs,alphabet, ++index) ;
+            rhs = "" + alphabet.charAt(index);  
+            return lhs + rhs;
         }
 
-    }
     
+    }
 
-//    public static String new_string(String s, String original, String[] alphabet, int index) {
-//        String lhs = "", rhs = "", test = ""; // l= left hand sid of string, r = right side (the last char)
-//        System.out.println(s);
-//
-//        // ADD check 's' has against original
-//        // check if s is empty
-//        if (s.length() > 0) {
-//            index = index % alphabet.length; // mod index by length of alphabet
-//            if (s.length() > 1) {
-//                lhs = s.substring(0, (s.length() - 1));
-//            }
-//            rhs.equals(s.substring(s.length() - 1));
-//
-//            //if rhs is not equal to last in the alaphbet
-//            if (rhs.equals(alphabet[alphabet.length - 1])) {
-//                index++;
-//                return new_string(lhs + alphabet[index], original, alphabet, index);
-//            } else {
-//                String s1 = lhs;
-//                if (index > 0 && s1.length() >1) {
-//                    lhs = s1.substring(0, (s1.length() - 1));
-//                    rhs =s1.substring(s1.length() - 1);
-//                }
-//                index++;
-//                new_string(lhs + alphabet[index], original, alphabet, index);
-//            }
-//
-//        } else {
-//            return new_string(s + alphabet[index], original, alphabet, index++);
-//        }
-
-  
-  
-  
-  
-        //
-        // test out the current string
-//        try {
-//            test = SHA1(s);
-//        } catch (UnsupportedEncodingException ex) {
-//            Logger.getLogger(Practical5.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        
-//        
-//        if(test == original)
-//            return s;
-//        else return new_string(s, original, alphabet);
-//        return s;
-//    }
 
     private static String convertToHex(byte[] data) {
         StringBuffer buf = new StringBuffer();
